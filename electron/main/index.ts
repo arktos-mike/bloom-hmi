d1613000fb89da4c78c2e9761bc7804dac5a5db7
arktos-mike <arktos.mike@gmail.com>
2022-12-26 09:51:49 +0300

    disable gpu

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -9,7 +9,8 @@ import './api/server'
 //==============================================================
 
 // Disable GPU Acceleration for Windows 7
-if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+//if (release().startsWith('6.1'))
+app.disableHardwareAcceleration()
 
 // Set application name for Windows 10+ notifications
 if (process.platform === 'win32') app.setAppUserModelId(app.getName())
