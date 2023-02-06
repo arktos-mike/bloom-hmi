94b4d92606dc0efd662df8ea94f6160e3ec3e9ca
Mike Semyonov <arktos.mike@gmail.com>
2023-02-06 15:19:18 +0300

    sync try to get value

--- electron/main/api/routes/datetime.ts
+++ electron/main/api/routes/datetime.ts
@@ -8,6 +8,34 @@ const options = {
 // it allows you to use async functions as route handlers
 const router = PromiseRouter();
 // export our router to be mounted by the parent application
+router.get('/', async (req, res) => {
+  try {
+    switch (process.platform) {
+        case 'linux':
+            //sudo.exec("date -s @" + unix + " && fake-hwclock save force", options, (error, data, getter) => {
+            sudo.exec("timedatectl", options, (error, data, getter) => {
+                if (!error) {
+                    res.status(200).json({
+                        data: data
+                    });
+                    console.log(data)
+                }
+            });
+            break;
+        case 'win32':
+
+            break;
+    }
+
+}
+catch (err) {
+    /*console.log(err);*/
+    res.status(500).json({
+        error: "Could not set system date/time",
+        message: "notifications.servererror",
+    });
+};
+})
 router.post('/', async (req, res) => {
     const { unix, iso, sync, tz } = req.body;
     console.log(iso, sync, tz)
